05d50c13f1c45ceb38c742065062acc7144807b1
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2019-10-22 00:38:15 +0200

    sphinxcontrib-qthelp: init at 1.0.2
    build dep of sphinx 2.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5017,6 +5017,8 @@ in {
   
   sphinxcontrib-jsmath = callPackage ../development/python-modules/sphinxcontrib-jsmath {};
 
+  sphinxcontrib-qthelp = callPackage ../development/python-modules/sphinxcontrib-qthelp {};
+
   sphinxcontrib-bibtex = callPackage ../development/python-modules/sphinxcontrib-bibtex {};
 
   sphinx-navtree = callPackage ../development/python-modules/sphinx-navtree {};
